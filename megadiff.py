fdcb04a71d8163ed87aaa387c3f1d77143c49089
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2009-08-14 22:01:31 +0000

    Made it so I can compare various runs to make sure they're numerically identical.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@426 3eb578c0-666c-0410-8428-3510748880ad

---
+++ megadiff.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# megadiff.py
+# Alex Szatmary
+# 2009-08-08
+# This is useful in comparing different runs with the same set of
+# parameters. The idea is that, while revising the code, megadiff can
+# be used when numerically identical results are expected from run to
+# run. This is useful when making stylistic changes to the code, but
+# not when implementing new physics.
+# To use, copy a directory from the batch directory to the main
+# directory of the repository, so you have a tree that looks something
+# like this:
+# cell/batch/myjob
+# cell/myjob
+# Run this script:
+# ./megadiff.py myjob
+# This will then diff a handful of files. If you see noise, you broke
+# something affecting the numerics.
+
+import os, time, os.path, sys
+
+wd = sys.argv[-1]
+
+files = ['TaylorDF__00001.txt', 'capsulev__00001.txt', 'capsulex__00001.txt', 'fort.206', 'fort.403', 'fort.451', 'rbcshpk0000.pro', 'solidforce00000.txt', 'solidforce00005.txt', 'solidforce00010.txt', 'solidnodes00000.txt', 'solidnodes00005.txt', 'solidnodes00010.txt', 'status.txt', 'uvwpdump__00000.txt', 'uvwpdump__00005.txt', 'uvwpdump__00010.txt', 'wprofile__00000.txt', 'wprofile__00001.txt', 'wprofile__00002.txt', 'wprofile__00003.txt', 'wprofile__00004.txt', 'wprofile__00005.txt', 'wprofile__00006.txt', 'wprofile__00007.txt', 'wprofile__00008.txt', 'wprofile__00009.txt', 'wprofile__00010.txt']
+for file in files:
+    os.system('diff batch/'+wd+'/'+file+' '+wd+'/'+file)
