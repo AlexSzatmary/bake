884377bdd4af680356560182d2298a5d634a9386
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2009-08-31 16:55:06 +0000

    Made it so the axes are drawn correctly
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@455 3eb578c0-666c-0410-8428-3510748880ad

--- profilemovie.raw.m
+++ profilemovie.raw.m
@@ -7,6 +7,7 @@ for k = 0:t_step:t_end
 		clear pro;
 		pro=load(sprintf('cappro%04d_%06d.txt',l,k), '-ascii');
 		plot(pro(:,1),pro(:,2),'.');
+		axis([0 32 0 32])
 		hold on;
 	end
 	M(k/t_step+1)=getframe;
