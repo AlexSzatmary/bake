2d0ab2e5962f27262cfb087f2d22292717e21e49
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2009-09-23 01:38:19 +0000

    I introduced a difference between nstep and nend. If they're the same, you do one run. If nstep is smaller than nend, you'd do this if you have several runs, the code figures out how many more steps to go, but stops at nend, regardless. You'd need to recompile the code or change nend to get it to go farther.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@510 3eb578c0-666c-0410-8428-3510748880ad

--- alex_python_experimental/rewrite.py
+++ alex_python_experimental/rewrite.py
@@ -5,10 +5,20 @@
 import os
 
 for root, dirs, files in os.walk('./bp'):
-    print dirs
     if '.svn' in dirs:
 	dirs.remove('.svn')
     for fname in files:
 	if '~' in fname:
 	    files.remove(fname)
     print root, dirs, files
+    for fname in files:
+        hin = open(os.path.join(root, fname))
+        lines = hin.readlines()
+        hin.close()
+        hout = open(os.path.join(root, fname), 'w')
+        for line in lines:
+            hout.write(line)
+            if ';$nend$;' in line:
+                line = line.replace('$nend$', '$nstep$')
+                hout.write(line)
+        hout.close()
