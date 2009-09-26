cadbbc40788e6ed3698fefbfb1119593d13c8c90
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2009-09-26 03:15:59 +0000

    Tested checkruns. It's super now!
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@515 3eb578c0-666c-0410-8428-3510748880ad

--- alex_python_experimental/checkruns.py
+++ alex_python_experimental/checkruns.py
@@ -8,8 +8,11 @@ tnorm = 60*60
 
 while runs != '':
     if runs != 'foo':
-	time.sleep()
-    runsssh = os.popen('ssh szatmary@pople.psc.edu qstat|grep szatmary')
-    runs = runsssh.read(random.uniform(tnorm*0.75, tnorm*1.25))
+	time.sleep(random.uniform(tnorm*0.75, tnorm*1.25))
+    runsssh = os.popen(
+        'ssh szatmary@pople.psc.edu qstat|grep szatmary')
+    runs = runsssh.read()
+    print runs
     runsssh.close()
-os.system("'growlnotify -n Runmonitor -s -m 'Pople runs done'")
+
+os.system("growlnotify -n Runmonitor -s -m 'Pople runs done'")
