fad45d4510df4d9be3b640928abc177cb442a663
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2009-10-01 19:00:55 +0000

    I made brelx.sh from br.sh because python isn't installed in the right place on eggletonlinux and I need to write more code to compensate for that.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@526 3eb578c0-666c-0410-8428-3510748880ad

---
+++ brelx.sh
@@ -0,0 +1,7 @@
+./batchrun.py bp/test/shear -r gfortran
+./batchrun.py bp/test/extensional -r gfortran
+./batchrun.py bp/test/drift -r gfortran
+./batchrun.py bp/test/mix -r gfortran
+./batchrun.py bp/test/poiseuille -r gfortran
+./batchrun.py bp/test/2cap -r gfortran
+./batchrun.py bp/test/collision5cap -r gfortran
