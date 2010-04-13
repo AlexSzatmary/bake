984272a1c8febb89c357d04cf75f2876467622bf
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-04-13 17:46:31 +0000

    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@623 3eb578c0-666c-0410-8428-3510748880ad

--- alex_python_experimental/rewrite.py
+++ alex_python_experimental/rewrite.py
@@ -19,23 +19,8 @@ for root, dirs, files in os.walk('./bp/'):
         hin.close()
         hout = open(os.path.join(root, fname), 'w')
         for line in lines:
-            if '$ncap$' not in line:
+            if '(//)' not in line:
                 hout.write(line)
             else:
-                hout.write(line.replace('$ncap$', '$nsph$'))
-                hout.write('$ncap$;($nsph$+$nellip$)\n')
-                hout.write('$nellip$;0\n')
-        hout.write('\n# Specify geometry for ellipses\n')
-        hout.write('# ellipa is the semi-major axis length in the n1 direction\n')
-        hout.write('# b in the n2, and c in the n3\n')
-        hout.write('$ellipa$;(//)\n')
-        hout.write('$ellipb$;(//)\n')
-        hout.write('$ellipc$;(//)\n')
-        hout.write('# Each ellipsoid has three entries in each of these arrays,\n')
-        hout.write('# ie, entries 1:3 in $ellipn2$ correspond to the n2 vector\n')
-        hout.write('# of the first ellipse, entries 4:6 correspond to the n2\n')
-        hout.write('# vector of the second ellipse, and so on.\n')
-        hout.write('$ellipn1$;(//)\n')
-        hout.write('$ellipn2$;(//)\n')
-        hout.write('$ellipn3$;(//)\n')
+                hout.write(line.replace('(//)', '(/0/)'))
         hout.close()
