dec8e7194ae3070a39d9927efb73f156b5179fb6
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-04-15 18:07:07 +0000

    Reworked default comments
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@632 3eb578c0-666c-0410-8428-3510748880ad

--- alex_python_experimental/rewrite.py
+++ alex_python_experimental/rewrite.py
@@ -4,6 +4,16 @@
 
 import os
 
+killlines = ['# ellipa is the semi-major axis length in the n1 direction\n',
+             '# b in the n2, and c in the n3\n',
+             '# Each ellipsoid has three entries in each of these arrays,\n',
+             '# ie, entries 1:3 in $ellipn2$ correspond to the n2 vector\n',
+             '# of the first ellipse, entries 4:6 correspond to the n2\n',
+             '# vector of the second ellipse, and so on.\n',
+             '$ellipn1$;(/0/)\n',
+             '$ellipn2$;(/0/)\n',
+             '$ellipn3$;(/0/)\n',]
+
 for root, dirs, files in os.walk('./bp/'):
     if '.svn' in dirs:
 	dirs.remove('.svn')
@@ -19,8 +29,22 @@ for root, dirs, files in os.walk('./bp/'):
         hin.close()
         hout = open(os.path.join(root, fname), 'w')
         for line in lines:
-            if '(//)' not in line:
-                hout.write(line)
+            if '# Specify geometry for ellipses' in line:
+                hout.write(
+                    """# Specify geometry for ellipses
+# ellipa, b, c describe the semi-principal axes; each is a vector going from 
+# the center to semi-principal points. These vectors should be orthogonal.
+# Each ellipsoid has three entries in each of these arrays,
+# ie, entries 1:3 in $ellipb$ correspond to the b vector
+# of the first ellipse, entries 4:6 correspond to the b
+# vector of the second ellipse, and so on.
+""")
             else:
-                hout.write(line.replace('(//)', '(/0/)'))
+                flag = 0
+                for killline in killlines:
+                    if killline == line:
+                        flag = 1
+                        break
+                if flag == 0:
+                    hout.write(line)
         hout.close()
