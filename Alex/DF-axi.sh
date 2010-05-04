94f6b8fa5069ff0442573119fd3cfe636783a1ee
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-04 04:02:43 +0000

    Little utility scripts for the axisymmetric cases
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@675 3eb578c0-666c-0410-8428-3510748880ad

---
+++ Alex/DF-axi.sh
@@ -0,0 +1,4 @@
+./batchrun.py bp/mix/uniaxi-planar-prolate -e DF
+./batchrun.py bp/mix/uniaxi-planar-oblate -e DF
+./batchrun.py bp/mix/biaxi-planar-prolate -e DF
+./batchrun.py bp/mix/biaxi-planar-oblate -e DF
---
+++ Alex/nvec-axi.sh
@@ -0,0 +1,4 @@
+./batchrun.py bp/mix/uniaxi-planar-prolate -e nvec1_0001.txt
+./batchrun.py bp/mix/uniaxi-planar-oblate -e nvec1_0001.txt
+./batchrun.py bp/mix/biaxi-planar-prolate -e nvec1_0001.txt
+./batchrun.py bp/mix/biaxi-planar-oblate -e nvec1_0001.txt
