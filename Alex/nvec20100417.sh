275fbdf5c53556327372674e2f48ca101bc0438c
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-05 00:06:36 +0000

    Renaming scripts for consistency
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@680 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/nvec20100417.sh
+++
@@ -1,8 +0,0 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -e nvec1_0001.txt
-./batchrun.py bp/mix/biaxi-planar-prolate -e nvec1_0001.txt
-./batchrun.py bp/mix/biaxi-planar-oblate -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.50d0" -o "\$phif\$;0.d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.5d0" -o "\$phif\$;0.d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e nvec1_0001.txt
--- Alex/shadow.sh
+++ Alex/shadow.sh
@@ -1,8 +1,4 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -e nvec1_0001.txt
-./batchrun.py bp/mix/biaxi-planar-prolate -e nvec1_0001.txt
-./batchrun.py bp/mix/biaxi-planar-oblate -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.50d0" -o "\$phif\$;0.d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.5d0" -o "\$phif\$;0.d0" -e nvec1_0001.txt
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e nvec1_0001.txt
+./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.25d0;0.50d0" -o "\$phif\$;0.00d0" -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.25d0;0.50d0" -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.25d0;0.50d0" -o "\$phif\$;0.00d0" -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.25d0;0.50d0" -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
