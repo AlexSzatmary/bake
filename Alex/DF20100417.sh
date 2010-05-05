5de7dd9defa18c393fe6e1abf08fb1298df1f7bf
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-05 00:05:19 +0000

    Made to use more decimal places
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@679 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/DF20100417.sh
+++ Alex/DF20100417.sh
@@ -1,8 +1,4 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate -e DF
-./batchrun.py bp/mix/uniaxi-planar-oblate -e DF
-./batchrun.py bp/mix/biaxi-planar-prolate -e DF
-./batchrun.py bp/mix/biaxi-planar-oblate -e DF
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.50d0" -o "\$phif\$;0.d0" -e DF
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e DF
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0;0.25d0;0.5d0" -o "\$phif\$;0.d0" -e DF
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.d0" -o "\$thetaf\$;0.d0" -o "\$phif\$;0.25d0;0.50d0" -e DF
+./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.25d0;0.50d0" -o "\$phif\$;0.00d0" -e DF
+./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.25d0;0.50d0" -e DF
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.25d0;0.5d0" -o "\$phif\$;0.00d0" -e DF
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.25d0;0.50d0" -e DF
