3c0a5147af65090be1b0c9fdb471c69e1bee3f1a
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-08 02:16:23 +0000

    Run script for l6 refinement
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@689 3eb578c0-666c-0410-8428-3510748880ad

---
+++ Alex/run-axi-l6.sh
@@ -0,0 +1,4 @@
+./batchrun.py bp/mix/uniaxi-planar-prolate -R tara
+./batchrun.py bp/mix/uniaxi-planar-oblate -R tara
+./batchrun.py bp/mix/biaxi-planar-prolate -R tara
+./batchrun.py bp/mix/biaxi-planar-oblate -R tara
