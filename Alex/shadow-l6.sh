6a3b6fbbddca7b8996c7fca09c3e5d6520591c04
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-11 16:45:10 +0000

    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@698 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/shadow-l6.sh
+++ Alex/shadow-l6.sh
@@ -1,4 +1,4 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -r tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -r tara
-./batchrun.py bp/mix/biaxi-planar-prolate-l6 -r tara
-./batchrun.py bp/mix/biaxi-planar-oblate-l6 -r tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/biaxi-planar-prolate-l6 -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
+./batchrun.py bp/mix/biaxi-planar-oblate-l6 -c "nvec* mv* Tay* caps* minmax* volume* lambda* mean*"
