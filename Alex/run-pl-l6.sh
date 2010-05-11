0bf3d9f9530859b5a777bcd082c6932a40b3716e
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-11 00:32:36 +0000

    Set up runs for l6 properly
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@695 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/run-pl-l6.sh
+++ Alex/run-pl-l6.sh
@@ -1,3 +1,2 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -o "\$phif\$;0.00d0" -r tara
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.50d0" -o "\$phif\$;0.00d0" -r tara
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.50d0" -r tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -o "\$r\$;1.0d0" -r tara
