aa38224190ccd1bfb5e61420c8a4240c6ed89d3d
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-11 02:58:17 +0000

    Analysis files for l6
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@696 3eb578c0-666c-0410-8428-3510748880ad

---
+++ Alex/nvec-l6.sh
@@ -0,0 +1,4 @@
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -r tara
+./batchrun.py bp/mix/biaxi-planar-prolate-l6 -r tara
+./batchrun.py bp/mix/biaxi-planar-oblate-l6 -r tara
