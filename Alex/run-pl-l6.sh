4e2f3e3febc73dcb82991198dd84c6d301736878
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-11 00:11:47 +0000

    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@693 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/run-pl-l6.sh
+++ Alex/run-pl-l6.sh
@@ -1,3 +1,3 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -o "\$phif\$;0.00d0" -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.50d0" -o "\$phif\$;0.00d0" -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.50d0" -R tara
+./batchrun.py bp/mix/uniaxi-planar-prolate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -o "\$phif\$;0.00d0" -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.50d0" -o "\$phif\$;0.00d0" -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0" -o "\$phif\$;0.50d0" -r tara
