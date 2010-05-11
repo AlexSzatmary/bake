5548ed14fc6cee0b17cd24471bb1ab43c7d2b1fe
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-11 16:46:08 +0000

    Consolidated my run cases into one file.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@700 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/run-l6.sh
+++ Alex/run-l6.sh
@@ -2,3 +2,5 @@
 ./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -R tara
 ./batchrun.py bp/mix/biaxi-planar-prolate-l6 -R tara
 ./batchrun.py bp/mix/biaxi-planar-oblate-l6 -R tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -R tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -o "\$r\$;1.0d0" -R tara
--- Alex/run-pl-l6.sh
+++
@@ -1,2 +0,0 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -o "\$r\$;1.0d0" -R tara
