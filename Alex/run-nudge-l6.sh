ebf4c8e7894205c15f9c9be68ad340ec2a01e964
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-23 03:11:39 +0000

    Set up refined runs for nudged ellipsoids
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@714 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/run-nudge-l6.sh
+++ Alex/run-nudge-l6.sh
@@ -1,6 +1,6 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate-nudge -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-nudge -R tara
-./batchrun.py bp/mix/biaxi-planar-prolate-nudge -R tara
-./batchrun.py bp/mix/biaxi-planar-oblate-nudge -R tara
-./batchrun.py bp/mix/uniaxi-planar-prolate-nudge -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.45d0" -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-nudge -o "\$r\$;1.0d0" -R tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-nudge-l6 -R tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-nudge-l6 -R tara
+./batchrun.py bp/mix/biaxi-planar-prolate-nudge-l6 -R tara
+./batchrun.py bp/mix/biaxi-planar-oblate-nudge-l6 -R tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-nudge-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.45d0" -R tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-nudge-l6 -o "\$r\$;1.0d0" -R tara
