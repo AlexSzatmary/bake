e714259247fcb2ff8dbef689db781cccd4df7405
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-26 22:19:42 +0000

    Sets up the data to make plots for varying r for ellipsoids and spheres.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@719 3eb578c0-666c-0410-8428-3510748880ad

---
+++ Alex/make-plots-varyr.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+./batchrun.py bp/mix/uniaxi-planar -p DF
+mv extractTaylorDF__00001.txt up-sp-DF.txt
+./batchrun.py bp/mix/uniaxi-planar-prolate-varyr -p DF
+mv extractTaylorDF__00001.txt up-pr-DF.txt
+./batchrun.py bp/mix/uniaxi-planar-oblate-varyr -p DF
+mv extractTaylorDF__00001.txt up-ob-DF.txt
+
+./batchrun.py bp/mix/biaxi-planar -p DF
+mv extractTaylorDF__00001.txt bp-sp-DF.txt
+./batchrun.py bp/mix/biaxi-planar-prolate-varyr -p DF
+mv extractTaylorDF__00001.txt bp-pr-DF.txt
+./batchrun.py bp/mix/biaxi-planar-oblate-varyr -p DF
+mv extractTaylorDF__00001.txt bp-ob-DF.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/uniaxi-planar -f
+grep '^tau* *=.* .*/.*' fit.log > up-sp-tau.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/uniaxi-planar-prolate-varyr -f
+grep '^tau* *=.* .*/.*' fit.log > up-pr-tau.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/uniaxi-planar-oblate-varyr -f
+grep '^tau* *=.* .*/.*' fit.log > up-ob-tau.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/biaxi-planar -f
+grep '^tau* *=.* .*/.*' fit.log > bp-sp-tau.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/biaxi-planar-prolate-varyr -f
+grep '^tau* *=.* .*/.*' fit.log > bp-pr-tau.txt
+
+rm -f fit.log
+./batchrun.py bp/mix/biaxi-planar-oblate-varyr -f
+grep '^tau* *=.* .*/.*' fit.log > bp-ob-tau.txt
+
+./batchrun.py bp/mix/uniaxi-planar -p volumearea00001.txt 
+mv extractvolumearea00001.txt up-sp-area.txt
+./batchrun.py bp/mix/uniaxi-planar-prolate-varyr -p volumearea00001.txt 
+mv extractvolumearea00001.txt up-pr-area.txt
+./batchrun.py bp/mix/uniaxi-planar-oblate-varyr -p volumearea00001.txt 
+mv extractvolumearea00001.txt up-ob-area.txt
+
+./batchrun.py bp/mix/biaxi-planar -p volumearea00001.txt 
+mv extractvolumearea00001.txt bp-sp-area.txt
+./batchrun.py bp/mix/biaxi-planar-prolate-varyr -p volumearea00001.txt 
+mv extractvolumearea00001.txt bp-pr-area.txt
+./batchrun.py bp/mix/biaxi-planar-oblate-varyr -p volumearea00001.txt 
+mv extractvolumearea00001.txt bp-ob-area.txt
