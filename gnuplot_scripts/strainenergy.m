812f601c373037ec2fecae93fe420d967109d393
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-05-31 19:12:05 +0000

    Making strain energy routine in Python so I won't need Matlab at all.
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@723 3eb578c0-666c-0410-8428-3510748880ad

--- gnuplot_scripts/strainenergy.m
+++ gnuplot_scripts/strainenergy.m
@@ -7,12 +7,12 @@ ca=['1.d-2'];
 w = zeros(size(ca, 1),size(r, 1));
 for i = 1:size(ca, 1)
   for j = 1:size(r, 1)
-%    cd(strcat('~/Documents/Linear flow/mix20100507-psc-shadow/batch/bp-ob-l5r',r(j,:),'g',ca(i,:), 't0.00d0p0.50d0'))
-    cd(strcat('~/Documents/Linear flow/mix20100507-psc-shadow/batch/bp-sp-l5r',r(j,:),'g',ca(i,:)))
-    load stretches_02000.txt
+%    cd(strcat('~/data/mix20100518-varyr-l6-shadow/batch/bp-pr-l6r',r(j,:),'g',ca(i,:), 't0.00d0p0.00d0'))
+    cd(strcat('~/data/mix20100518-varyr-l6-shadow/batch/bp-sp-l6r',r(j,:),'g',ca(i,:)))
+    load stretches_04000.txt
     load shpa0001.txt
-    for k=1:size(stretches_02000,1)
-      w(i,j) = w(i,j) + (stretches_02000(k,2)^2+stretches_02000(k,3)^2+stretches_02000(k,2)^-2*stretches_02000(k,3)^-2-3)*shpa0001(k,4)/3;
+    for k=1:size(stretches_04000,1)
+      w(i,j) = w(i,j) + (stretches_04000(k,2)^2+stretches_04000(k,3)^2+stretches_04000(k,2)^-2*stretches_04000(k,3)^-2-3)*shpa0001(k,4)/3;
     end
     w(i,j) = w(i,j)/sum(shpa0001(:,4));
   end
