05fd6583ea97fafc0fe983baeba45fb1fd4c4993
al1 <al1@3eb578c0-666c-0410-8428-3510748880ad>
2010-10-22 21:12:28 +0000

    Set up runs for alignment on a fancy grid
    git-svn-id: svn+ssh://eggletonlinux.meng.umbc.edu/scratch/linux1/svn_repo/trunk/immersed_boundary/cell@795 3eb578c0-666c-0410-8428-3510748880ad

--- Alex/run-l6.sh
+++ Alex/run-l6.sh
@@ -1,6 +1,6 @@
-./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -R tara
-./batchrun.py bp/mix/biaxi-planar-prolate-l6 -R tara
-./batchrun.py bp/mix/biaxi-planar-oblate-l6 -R tara
-./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -R tara
-./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -o "\$r\$;1.0d0" -R tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -r tara
+./batchrun.py bp/mix/biaxi-planar-prolate-l6 -r tara
+./batchrun.py bp/mix/biaxi-planar-oblate-l6 -r tara
+./batchrun.py bp/mix/uniaxi-planar-prolate-l6 -o "\$r\$;1.0d0" -o "\$thetaf\$;0.00d0;0.50d0" -r tara
+./batchrun.py bp/mix/uniaxi-planar-oblate-l6 -o "\$r\$;1.0d0" -r tara
