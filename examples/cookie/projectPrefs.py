f5eed8e8556222cc13cc670d5769c5d112ba479d
Alex Szatmary <alex.szatmary@gmail.com>
2011-01-06 21:43:24 -0500

    Make cookie example able to use revised bake
    In particular, many options are moved from projectPrefs.py to
    cookie.cfg.

--- examples/cookie/projectPrefs.py
+++ examples/cookie/projectPrefs.py
@@ -7,14 +7,4 @@ def set_opt_parser(optparser):
     pass
 
 class InitializeOptions:
-    # Define which files need tweaking
-    code_files = ['cookie.txt']
-    file_in_suffix = ''
-    file_out_suffix = ''
-    label_tag = '@label@'
-    pattern = re.compile('@.+?@')
-
-    # These files are intended to make visualization with Matlab easier.
-    visual_files = []
-    visual_file_in_suffix = ''
-    visual_file_out_suffix = ''
+    pass
--- examples/cookie/test.py
+++ examples/cookie/test.py
@@ -2,21 +2,29 @@
 
 import os
 try:
-    p = os.popen('../../bake.py -m bp/many_sprinkles.txt')
+    p = os.popen('../../bake.py -m -l -f bp/many_sprinkles.txt')
     hout = open('batch/test1.txt', 'w')
     hout.write(p.read())
     hout.close()
     p.close()
-    p = os.popen('../../bake.py -l bp/many_sprinkles.txt -s 2-4')
+    p = os.popen('../../bake.py -l -f bp/many_sprinkles.txt -s 2-4')
     hout = open('batch/test2.txt', 'w')
     hout.write(p.read())
     hout.close()
     p.close()
-    p = os.popen('../../bake.py -m bp/many_sprinkles.txt -o "@color@;purple"')
+    p = os.popen('../../bake.py -m -l -f bp/many_sprinkles.txt -o "@color@;purple"')
     hout = open('batch/test3.txt', 'w')
     hout.write(p.read())
     hout.close()
+    p.close() 
+    p = os.popen('../../bake.py -f bp/many_sprinkles.txt -e "cat cookie.txt"')
+    hout = open('batch/test4.txt', 'w')
+    hout.write(p.read())
+    hout.close()
     p.close()
+    
+
+    # diff tests with reference case
     p = os.popen('for f in `ls batch`; do echo $f; diff -r batch/$f test/$f; done')
     print p.read()
     p.close()
