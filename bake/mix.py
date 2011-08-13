e2cf210daa22d2c67779dacbcd325f78ac4ea5c3
Alex Szatmary <alex.szatmary@gmail.com>
2011-08-13 14:26:48 -0400

    Make pep8 happy

--- bake/mix.py
+++ bake/mix.py
@@ -6,24 +6,30 @@
 # them, mix and match them, and so on.
 import re
 
-# This takes each token's value and expands the values by substituting in
-# the values of other tokens.
+
 def TokenValueSubValue(values, tokendict, pattern):
+    """
+    This takes each token's value and expands the values by substituting in
+    the values of other tokens.
+    """
     for j in range(len(values)):
         foundtoken = re.search(pattern, values[j])
         # This is done iteratively so that it doesn't matter what order lines
         # appear in a bake parameter file
         while foundtoken:
-            values[j] = values[j].replace(foundtoken.group(0),
-                                          values[tokendict[foundtoken.group(0)]])
+            values[j] = values[j].replace(
+                foundtoken.group(0), values[tokendict[foundtoken.group(0)]])
             foundtoken = re.search(pattern, values[j])
     return None
 
-# This iterator returns one set of values for each run; one run is given
-# by each iteration of ItRunValues. These values can then be subbed in for
-# the corresponding tokens.
+
 def ItRunValues(list_values, tokens, n_values, N_values, pattern, tokendict,
                 slice_start=0, slice_end=0):
+    """
+    This iterator returns one set of values for each run; one run is given
+    by each iteration of ItRunValues. These values can then be subbed in for
+    the corresponding tokens.
+    """
     if slice_end == 0:
         slice_end = N_values
         #listi and values need to be initialized
@@ -36,6 +42,7 @@ def ItRunValues(list_values, tokens, n_values, N_values, pattern, tokendict,
         TokenValueSubValue(values, tokendict, pattern)
         yield values
 
+
 # This is basically a thing that encloses ItList_iNoSlice and does the slicing
 # math for it.
 def ItList_i(n_values, slice_start, slice_end):
@@ -44,6 +51,7 @@ def ItList_i(n_values, slice_start, slice_end):
         myItList_iNoSlice.next()
     for i in xrange(slice_start, slice_end):
         yield myItList_iNoSlice.next()
+
 
 # Imagine an m-dimensional grid, where m is the number of tokens; the width of
 # the grid in one of these directions is the number of values for that token.
@@ -63,16 +71,19 @@ def ItList_iNoSlice(n_values):
                 break
         yield list_i
 
-# This function takes lines from a bp file, plus files from options.overwrite
-# It returns a freakish tuple with:
-# tokes: the list of tokens
-# list_values: the list of values for a given token (these are matched by their
-#   indices)
-# n_values is the number of values for a given index
-# N_values is the repeated product of n_values; it is the total number of
-#   runs to do.
-# tokendict gives the index of a given token
+
 def parseBPlines(lines):
+    """
+    This function takes lines from a bp file, plus files from options.overwrite
+    It returns a freakish tuple with:
+    tokes: the list of tokens
+    list_values: the list of values for a given token (these are matched by
+    their indices)
+    n_values is the number of values for a given index
+    N_values is the repeated product of n_values; it is the total number of
+    runs to do.
+    tokendict gives the index of a given token
+    """
     # Load bp file
     tokens = []
     list_values = []
@@ -83,17 +94,17 @@ def parseBPlines(lines):
     m = 0
     for line in lines:
         if (line[0] != '#'):
-            line = line.replace('\n','').replace('\\n','&\n')
+            line = line.replace('\n', '').replace('\\n', '&\n')
             elements = line.split(';')
             if elements[0] not in tokens:
                 tokens.append(elements[0])
                 list_values.append(elements[1:])
-                n_values.append(len(elements)-1)
+                n_values.append(len(elements) - 1)
                 tokendict[tokens[-1]] = m
                 m = m + 1
 
-# Count how many runs I'm going to start
+    # Count how many runs I'm going to start
     N_values = 1
     for i in n_values:
-        N_values = N_values*i
+        N_values = N_values * i
     return (tokens, list_values, n_values, N_values, tokendict)
