452ad6f3de797285a50094a4a145714e75204d95
Alex Szatmary <alex.szatmary@gmail.com>
2011-08-13 13:47:33 -0400

    Make pep8 run mostly cleanly

--- bake/cmdline.py
+++ bake/cmdline.py
@@ -8,7 +8,7 @@
 import api as bake
 import sys
 
-# This 
+
 def main(args=sys.argv[1:]):
   # Set up command line argument options
   optparser = bake.make_optparser()
@@ -29,17 +29,17 @@ def main(args=sys.argv[1:]):
     lines = []
 
   # Load bake parameter file
-  hin = open(options.file,'r')
+  hin = open(options.file, 'r')
   lines += hin.readlines()
   hin.close()
 
   # This mixIterator object is kind of the core of bake.
-  (label, tokens, 
+  (label, tokens,
    mixIterator) = bake.make_iterator(config['label']['label_tag'],
-                                     config['label']['pattern'], 
-                                     lines, options.slice_start, 
+                                     config['label']['pattern'],
+                                     lines, options.slice_start,
                                      options.slice_end)
-   
+
   ## This is the main loop, iterating over each set of values
   bake.default_loop(label, tokens, mixIterator, config, options)
 
