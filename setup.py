8b370d57bd874c6b5e4474b9d1a845d856374d20
Alex C. Szatmary <alex.szatmary@gmail.com>
2011-08-16 16:21:12 -0400

    Get set up to submit to github

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "bake",
-    version = "0.0",
+    version = "0.0.0",
     packages = find_packages(),
     entry_points="""
     # -*- Entry points: -*-
