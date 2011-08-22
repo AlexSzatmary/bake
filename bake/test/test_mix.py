5d67010a0ea8e8f23495c8c7aa2d972f1e0cd547
Alex C. Szatmary <alex.szatmary@gmail.com>
2011-08-22 14:32:18 -0400

    Make pep8 happy, remove mixed tabs and spaces

--- bake/test/test_mix.py
+++ bake/test/test_mix.py
@@ -5,12 +5,13 @@ import load
 import os
 import mix
 
+
 class parseBPlinesTestCase(unittest.TestCase):
     def test_overwrite(self):
-	# Test that a later line overwrites a previous line
-	grid = mix.parseBPlines(['@foo@;bar;baz', '@foo@;rag'])
-	self.assertEqual(grid.tokens[0], '@foo@')
-	self.assertEqual(grid.list_values[0][0], 'rag')
+        # Test that a later line overwrites a previous line
+        grid = mix.parseBPlines(['@foo@;bar;baz', '@foo@;rag'])
+        self.assertEqual(grid.tokens[0], '@foo@')
+        self.assertEqual(grid.list_values[0][0], 'rag')
 
 if __name__ == '__main__':
     unittest.main()
