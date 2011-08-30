365d1ff83b5d36ec27c2d301f28620159d7e42f9
Alex C. Szatmary <alex.szatmary@gmail.com>
2011-08-30 16:05:10 -0400

    Reindent

--- bake/test/test_load.py
+++ bake/test/test_load.py
@@ -4,27 +4,29 @@ import unittest
 import load
 import os
 
+
 class loadLinesTestCase(unittest.TestCase):
     def test_load(self):
-	# Test line continuation
-	self.assertEqual(load.load(x for x in ['l1\\', '\tl2\\  ', '\tl3'])[0],
-			 'l1l2l3')
+        # Test line continuation
+        self.assertEqual(load.load(x for x in ['l1\\', '\tl2\\  ', '\tl3'])[0],
+                         'l1l2l3')
+
 
 class loadFileTestCase(unittest.TestCase):
     def setUp(self):
-	self.cd = cd = os.getcwd()
-	os.chdir('../examples/cookie/bp/')
+        self.cd = cd = os.getcwd()
+        os.chdir('../examples/cookie/bp/')
 
     def test_load_file(self):
-	with open('include3.txt') as f:
-	    self.assertEqual(
-		load.load(f),
-		['@label@;include1', '@label@;cookie-decoration-@decoration@', 
-		 '@type@;sugar', '@nuts@;none', '@decoration@;none', 
-		 '@cutter@;circle'])
+        with open('include3.txt') as f:
+            self.assertEqual(
+                load.load(f),
+                ['@label@;include1', '@label@;cookie-decoration-@decoration@',
+                 '@type@;sugar', '@nuts@;none', '@decoration@;none',
+                 '@cutter@;circle'])
 
     def tearDown(self):
-	os.chdir(self.cd)
+        os.chdir(self.cd)
 
 if __name__ == '__main__':
     unittest.main()
