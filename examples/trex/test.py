08148593980d0c0abdf055610e7065ae41452aa7
Alex C. Szatmary <alex.szatmary@gmail.com>
2011-09-01 21:47:00 -0400

    Use -b option, expand explanation

--- examples/trex/test.py
+++ examples/trex/test.py
@@ -3,30 +3,33 @@
 import os
 
 bakecmd = '../../bake/cmdline.py'
-dashos =  "-o '@possessive@;your' -o '@desire@;love' -o '@location@;beachhouse' -o '@adjective1@;amazing'"
+dashos = "-o '@possessive@;your' -o '@desire@;love' " + \
+    "-o '@location@;beachhouse' -o '@adjective1@;amazing'"
 my_slice = '-s 121-144'
 
-p = os.popen(bakecmd + " -l -f trex.bp " + dashos + "|wc -l")
+p = os.popen(bakecmd + " -l -f trex.bp -b trex.txt " + dashos + "|wc -l")
 hout = open('batch/test-count-jobs.txt', 'w')
 hout.write(p.read())
 hout.close()
 p.close()
-p = os.popen(bakecmd + " -l -f trex.bp " + dashos)
+p = os.popen(bakecmd + " -l -f trex.bp -b trex.txt " + dashos)
 hout = open('batch/test-list.txt', 'w')
 hout.write(p.read())
 hout.close()
 p.close()
-p = os.popen(bakecmd + " -l -f trex.bp " + my_slice + " " + dashos)
+p = os.popen(bakecmd + " -l -f trex.bp -b trex.txt " + my_slice + " " + dashos)
 hout = open('batch/test-slice.txt', 'w')
 hout.write(p.read())
 hout.close()
 p.close()
-p = os.popen(bakecmd + " -l -f trex.bp " + my_slice + " -m -b trex.txt " + dashos)
+p = os.popen(bakecmd + " -l -f trex.bp -b trex.txt " + my_slice +
+             " -m -b trex.txt " + dashos)
 hout = open('batch/test-mix.txt', 'w')
 hout.write(p.read())
 hout.close()
 p.close()
-p = os.popen(bakecmd + " -l -f trex.bp " + my_slice + " " + dashos + " -e 'cat trex.txt'")
+p = os.popen(bakecmd + " -l -f trex.bp -b trex.txt " + my_slice + " " +
+             dashos + " -e 'cat trex.txt'")
 hout = open('batch/test-execute.txt', 'w')
 hout.write(p.read())
 hout.close()
